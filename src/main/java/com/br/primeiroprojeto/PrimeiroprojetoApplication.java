e04606ce7b0d8c5cf7473283c73e3c9b6c0be55b
Wellington Chaves <chaves_ws@hotmail.com>
2018-03-12 16:21:44 -0300

    Projeto Criado

---
+++ src/main/java/com/br/primeiroprojeto/PrimeiroprojetoApplication.java
@@ -0,0 +1,12 @@
+package com.br.primeiroprojeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroprojetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroprojetoApplication.class, args);
+	}
+}
